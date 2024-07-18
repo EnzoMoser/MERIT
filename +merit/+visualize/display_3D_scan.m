a608e9df7fc958813c6471f33bcb74a54ee4113f
Enzo Moser <irlenzom@gmail.com>
2024-07-18 10:56:41 +0000

    Update

--- +merit/+visualize/display_3D_scan.m
+++ +merit/+visualize/display_3D_scan.m
@@ -1,23 +1,39 @@
 function display_3D_scan(grid_, options)
+% Display a 3D render of an (:, :, :) grid.
+% The density is relative to the highest and lowest numbers.
 arguments
-    grid_ (:, :, :)
+    % Matrix containing the data to render.
+    grid_ (:, :, :) {mustBeNumeric}
 
-    % Specify figure to display
-    options.figure_number (1, 1) {mustBeNumeric} ...
-        = 0 % Default value is don't specify.
+    % --- Optional arguments---
 
-    % Shift the opacity of the upper quartile between 0 and 1.
+    % Specify figure to display. "0" means don't specify
+    options.figure_number (1, 1) {mustBeGreaterThanOrEqual(options.figure_number, 0)} ...
+        = 0
+
+    % Change the factor (between 0 and 1) for the max_opactiy of...
+    % numbers from the upper quartile to the 90th quartile...
+    % ( Default is 1 ).
+    options.upper_opacity (1, 1)...
+        {mustBeInRange(options.upper_opacity, 0, 1, 'exclude-lower')} ...
+        = 1
+
+    % Shift the max_opacity for numbers between the lower and...
+    % upper quartile (between 0 and 1).
     % The pixels will/should never be fully opaque, as that just looks bad.
-    options.upper_opacity (1, 1) {mustBeNumeric, mustBePositive, ...
-        mustBeLessThanOrEqual(options.upper_opacity, 1)} ...
+    options.middle_opacity_factor (1, 1) ...% Default value
+        {mustBeInRange(options.middle_opacity_factor, 0, 1, 'exclude-lower')} ...
         = 0.4 % Default value
 
-    % Change the factor for the opacity of the lower quartile by a...
-    % factor of the upper_opactiy
-    % lower_opacity = upper_opacity * lower_opacity_factor
-    options.lower_opacity_factor (1, 1) {mustBeNumeric, mustBePositive, ...
-        mustBeLessThanOrEqual(options.lower_opacity_factor, 1)} ...
-        = 0.1 % Default value
+    % Change the factor (between 0 and 1) for the max_opacity of...
+    % numbers below the lower quartile by a factor of the upper_opactiy.
+    % max_lower_opacity = middle_opacity * lower_opacity_factor
+    options.lower_opacity_factor (1, 1) ...
+        {mustBeInRange(options.lower_opacity_factor, 0, 1, 'exclude-lower')} ...
+        = 0.1
+    
+    % Specify the colormap. Can take a string or a (:, 3) matrix
+    options.colormap_ = "jet"
 end
 
 if options.figure_number < 1
@@ -43,7 +59,7 @@ set(hy, 'EdgeColor', 'none');
 hx = slice(x, y, z, grid_, 1:lx, [], []);
 set(hx, 'EdgeColor', 'none');
 
-colormap jet; % Set the colormap to 'jet'
+colormap(options.colormap_);
 colorbar; % Show color bar to indicate density values
 xlabel('X-axis');
 ylabel('Y-axis');
@@ -62,25 +78,31 @@ min_value = min(vector_grid);
 
 Q1 = quantile(vector_grid, 0.25);
 Q3 = quantile(vector_grid, 0.75);
+Q99 = quantile(vector_grid, 0.90);
 
 % Determine the values at specific steps
 st = min_value;
 md1 = Q1;
 md2 = Q3;
+md3 = Q99;
 ed = max_value;
 
-sep2 = options.upper_opacity;
-
+sep3 = options.upper_opacity;
+sep2 = sep3 * options.middle_opacity_factor;
 sep1 = sep2 * options.lower_opacity_factor;
-stps1 = ( ed - st) / ( md1 - st );
-stps2 = ( ed - st) / ( md2 - md1 );
-stps3 = ( ed - st) / ( ed - md2 );
+full_diff = ( ed - st);
+
+stps1 = full_diff / ( md1 - st );
+stps2 = full_diff / ( md2 - md1 );
+stps3 = full_diff / ( md3 - md2 );
+stps4 = full_diff / ( ed - md3 );
 
 % Adjust alpha_map to reflect the relative differences
 alpha_map = [ ...
     linspace( 0.01, sep1, stps1 ) ...
     linspace( sep1, sep2, stps2 )  ...
-    linspace( sep2, 1, stps3 ) ...
+    linspace( sep2, sep3, stps3 )  ...
+    linspace( sep3, 1, stps4 ) ...
     ];
 
 % Apply the custom alpha map
