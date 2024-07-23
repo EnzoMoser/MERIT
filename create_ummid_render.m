94630ee6056f1f5ec7a9cb62b047f95d3ee52467
Enzo Moser <irlenzom@gmail.com>
2024-07-23 10:44:43 +0000

    Update ummid

--- create_ummid_render.m
+++ create_ummid_render.m
@@ -1,25 +1,25 @@
-scan_num = 45;
+scan_num = 45; % 45 and 71 are good tests.
 
-metadata = load('../um_bmid/datasets/gen-two/clean/md_list_s21_adi.mat');
+metadata = load('../um_bmid/datasets/gen-three/clean/md_list_s21_adi.mat');
 % Extract the metadata array from the struct dytpe it is stored in
 metadataFieldName = fieldnames(metadata);
 metadata = getfield(metadata, metadataFieldName{1}); %#ok<GFLD>
 
-scan_data = load('../um_bmid/datasets/gen-two/clean/fd_data_s11_adi.mat');
+scan_data = load('../um_bmid/datasets/gen-three/clean/fd_data_s11_adi.mat');
 % Extract the data array from the struct dtype it is stored in
 scanDataFieldName = fieldnames(scan_data);
 scan_data = getfield(scan_data, scanDataFieldName{1}); %#ok<GFLD>
 
 scan1 = squeeze(scan_data(scan_num, :, :));
 
-scan_data = load('../um_bmid/datasets/gen-two/clean/fd_data_s21_adi.mat');
+scan_data = load('../um_bmid/datasets/gen-three/clean/fd_data_s21_adi.mat');
 % Extract the data array from the struct dtype it is stored in
 scanDataFieldName = fieldnames(scan_data);
 scan_data = getfield(scan_data, scanDataFieldName{1}); %#ok<GFLD>
 
 scan2 = squeeze(scan_data(scan_num, :, :));
 
-signals = scan1 + scan2;
+signals = scan2;
 
 frequencies = linspace( 1e9, 8e9, size(scan1, 1) );
 radius = metadata{scan_num}.ant_rad * 1e-2; % The radius of the scan.
@@ -28,22 +28,26 @@ number_antennas = size(scan1, 2); % The number of antenna locations.
 antenna_angles = (linspace(0, (1 - (1/number_antennas) ) * 2 * pi, number_antennas)); % If number_antennas = 72, then steps of 5 from 0 to 355 (but in radians)
 antenna_locations = permute ( [ ( cos(antenna_angles) * radius ); ( sin(antenna_angles) * radius ) ], [2,1] );
 
+channel_one = 1:number_antennas;
+channel_two = mod ( channel_one+11, number_antennas) + 1; % The receiving antenna is 60 degrees ahead of the transmitting antenna
+channel_names = permute ( [ channel_one; channel_two ], [2, 1] );
+
 %% Plot the acquired scans.
 figure;
-data_channel1 = [ scan1(:, 1), scan2(:, 1), signals(:, 1)];
+data_channel1 = [ signals(:, 1)];
 channel1_magnitude = mag2db(abs(data_channel1));
 channel1_phase = unwrap(angle(data_channel1));
 subplot(2, 1, 1);
 plot(frequencies, channel1_magnitude);
 xlabel('Frequency (Hz)');
 ylabel('Magnitude (dB)');
-legend('s11', 's21', 'signals (s11+s21)');
+legend('s21');
 title(sprintf('Channel (%d, %d) Magnitude', channel_names(1, :)));
 subplot(2, 1, 2);
 plot(frequencies, channel1_phase);
 xlabel('Frequency (Hz)');
 ylabel('Phase (rad)');
-legend('s11', 's21', 'signals (s11+s21)');
+legend('s21');
 title(sprintf('Channel (%d, %d) Phase', channel_names(1, :)));
 % 
 % Generate imaging domain
@@ -52,10 +56,6 @@ title(sprintf('Channel (%d, %d) Phase', channel_names(1, :)));
 % Calculate delays
 % merit.get_delays returns a function that calculates the delay
 %   to each point from every antenna.
-
-channel_one = 1:number_antennas;
-channel_two = mod ( channel_one+11, number_antennas) + 1; % The receiving antenna is 60 degrees ahead of the transmitting antenna
-channel_names = permute ( [ channel_one; channel_two ], [2, 1] );
 
 delays = merit.beamform.get_delays(channel_names, antenna_locations, ...
   'relative_permittivity', 8);
