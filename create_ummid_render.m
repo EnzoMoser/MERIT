136e3cb1baea567f6e1150bbe48104d34278100c
Enzo Moser <irlenzom@gmail.com>
2024-07-30 14:17:22 +0000

    Now works will all signals

--- create_ummid_render.m
+++ create_ummid_render.m
@@ -1,6 +1,8 @@
 clear
 clc
 
+m_size = 150;
+
 %% Choose scan number to work on
 scan_num = 1;
 
@@ -15,17 +17,13 @@ ADI_RADS.A15 = 5.53e-2;
 ADI_RADS.A16 = 5.74e-2;
 
 %% Load data
-metadata = load('../um_bmid/datasets/gen-three/simple-clean/matlab-data/metadata_gen_three.mat');
+metadata = load('../um_bmid/datasets/gen-three/clean/md_list_s11_adi.mat');
 % Extract the metadata array from the struct dytpe it is stored in
 metadataFieldName = fieldnames(metadata);
 metadata = getfield(metadata, metadataFieldName{1}); %#ok<GFLD>
 org_metadata = metadata{scan_num};
 
-if isnan(org_metadata.tum_diam)
-    error('This scan does not have a tumor! Select a different scan!');
-end
-
-scan_data = load('../um_bmid/datasets/gen-three/simple-clean/matlab-data/fd_data_gen_three_s11.mat');
+scan_data = load('../um_bmid/datasets/gen-three/clean/fd_data_s11_adi.mat');
 % Extract the data array from the struct dtype it is stored in
 scanDataFieldName = fieldnames(scan_data);
 scan_data = getfield(scan_data, scanDataFieldName{1}); %#ok<GFLD>
@@ -50,30 +48,32 @@ scan_data = scan_data(:, 1:sample_divide:end, :);
 % Get the full signal
 org_signal = squeeze(scan_data(scan_num, :, :));
 
-% Get the adipose-only version.
-adi_signal = squeeze(scan_data(org_metadata.adi_ref_id, :, :));
+if isnan(org_metadata.tum_diam)
+    disp('This scan does NOT have a tumor!');
+end
 
-cropped_signal = org_signal - adi_signal;
+% Get the adipose-only version.
+    cropped_signal =org_signal;
 
 % Get the adipose-plus-fiboglandular-only version (everything except tumor)
 %adifib_signal = squeeze(scan_data(org_metadata.fib_ref_id, :, :));
 
 %% Plot the acquired scans.
 figure;
-data_channel1 = [ org_signal(:, 1), cropped_signal(:, 1) ] ;
+data_channel1 = [ cropped_signal(:, 1) ] ;
 channel1_magnitude = mag2db(abs(data_channel1));
 channel1_phase = unwrap(angle(data_channel1));
 subplot(2, 1, 1);
 plot(frequencies, channel1_magnitude);
 xlabel('Frequency (Hz)');
 ylabel('Magnitude (dB)');
-legend('signal', 'clean signal');
+legend('clean signal');
 %title(sprintf('Channel (%d, %d) Magnitude', channel_names(1, :)));
 subplot(2, 1, 2);
 plot(frequencies, channel1_phase);
 xlabel('Frequency (Hz)');
 ylabel('Phase (rad)');
-legend('signal', 'clean signal');
+legend('clean signal');
 %title(sprintf('Channel (%d, %d) Phase', channel_names(1, :)));
 % 
 
@@ -84,23 +84,54 @@ sig_rad = ant_rad + 2.4e-2; % "2.4cm" is by Reimer. The distance from the centre
 split_adi_id = strsplit(org_metadata.phant_id, 'F'); % Split the string after every "F".
 adi_rad = ADI_RADS.(split_adi_id{1}); % Radius of the adi-pose. For some reason, it is not included in the metadata.
 
+roi_rad = 8e-2; % The radius of information is 8cm.
+
+%Define the x/y points on each axis
+full_xs = linspace(-sig_rad, sig_rad, m_size);
+full_ys = linspace(sig_rad, -sig_rad, m_size);
+
+% Cast these to 2D for ease later
+[ full_pix_xs, full_pix_ys ] = meshgrid(full_xs, full_ys);
+
+% Find the distance from each pixel to the center of the model space
+pix_dist_from_center = sqrt(full_pix_xs.^2 + full_pix_ys.^2);
+
+% Get the region of interest as all the pixels inside the
+% circle-of-interest
+full_in_roi = false([m_size, m_size]);
+full_in_roi(pix_dist_from_center < sig_rad) = true;
+
 % % This is needed to calculate the permitivity and hence speed, but for
 % % now we're skipiing this part.
-% air_permitivity = 1;
-% adi_permitivity = 7.08;
-% fib_permitivity = 44.94;
-% tum_permitivity = 77.11;
-% skin_permitivity = 40;
-% skin_thickness = 0;
-% 
-% pixel_dist_from_adi = [];
-% pixel_size = 0;
-% breast_model = air_permitivity * ones(pixel_size);
-% breast_model[ (pixel_dist_from_adi < adi_rad) ] = skin_perm;
+air_permitivity = 1;
+adi_permitivity = 7.08;
+tum_permitivity = 77.11;
+skin_permitivity = 40;
+skin_thickness = 0;
 
-prop_speed = 288739680.6368426; % Ripped it from the Python calculation
+tum_x = org_metadata.tum_x * 1e-2;
+tum_rad = org_metadata.tum_diam * 1e-2 / 2;
+tum_y = org_metadata.tum_y * 1e-2;
+adi_x = org_metadata.adi_x * 1e-2;
+adi_y = org_metadata.adi_y * 1e-2;
 
-av_perm = 1.07802408105191434369; % ( [speed of light] / prop_speed )^2     Taken from get_delays
+% Compute the pixel distances from the center of each tissue
+% component (excluding skin)
+pix_dist_from_adi = sqrt((full_pix_xs - adi_x).^2 + (full_pix_ys - adi_y).^2);
+pix_dist_from_tum = sqrt((full_pix_xs - tum_x).^2 + (full_pix_ys - tum_y).^2);
+
+% Initialize breast model to be uniform background medium
+breast_model = air_permitivity .* ones([m_size, m_size]);
+
+% Assign the tissue components
+breast_model(pix_dist_from_adi < (adi_rad + skin_thickness) ) = skin_permitivity;
+breast_model(pix_dist_from_adi < adi_rad) = adi_permitivity;
+breast_model(pix_dist_from_tum < tum_rad) = tum_permitivity;
+
+vac_speed = 3e8;
+
+prop_speed = mean( ( vac_speed ./ sqrt( breast_model(full_in_roi) ) ), "all" );
+% 288739680.6368426; % Ripped it from the Python calculation
 
 number_antennas = size(scan_data, 3); % The number of antenna locations.
 
@@ -110,15 +141,19 @@ antenna_angles = antenna_angles + starting_antenna_angle; % The starting antenna
 
 antenna_locations = permute ( [ ( cos(antenna_angles) * sig_rad ); ( sin(antenna_angles) * sig_rad ) ], [2,1] );
 
-roi_rad = 8e-2; % The radius of information is 8cm.
-
-m_size = 250;
 %Define the x/y points on each axis
 xs = linspace(-roi_rad, roi_rad, m_size);
 ys = linspace(roi_rad, -roi_rad, m_size);
 
-% Cast these to 2D for ease later
+% Cast these to 2D for ease latersig_rad
 [ pix_xs, pix_ys ] = meshgrid(xs, ys);
+% Find the distance from each pixel to the center of the model space
+pix_dist_from_center = sqrt(pix_xs.^2 + pix_ys.^2);
+
+% Get the region of interest as all the pixels inside the
+% circle-of-interest
+in_roi = false([m_size, m_size]);
+in_roi(pix_dist_from_center < roi_rad) = true;
 
 % Get the ant x/y positions
 ant_xs = antenna_locations(:, 2);
@@ -163,14 +198,6 @@ das_adi_recon = squeeze(das_adi_recon);
 img = abs(das_adi_recon);
 
 img_to_plt = img .* ones(size(img), "like", img);
-
-% Find the distance from each pixel to the center of the model space
-pix_dist_from_center = sqrt(pix_xs.^2 + pix_ys.^2);
-
-% Get the region of interest as all the pixels inside the
-% circle-of-interest
-roi = false(m_size);
-in_roi(pix_dist_from_center < roi_rad) = true;
 
 % Set the pixels outside the antenna trajectory to NaN
 img_to_plt( ~in_roi ) = nan;
