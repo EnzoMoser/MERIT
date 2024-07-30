aa83885f67390ccf540376a748d58b585e321d53
Enzo Moser <irlenzom@gmail.com>
2024-07-30 16:51:46 +0000

    Made functions for um_bmid

--- create_ummid_render.m
+++ create_ummid_render.m
@@ -14,8 +14,11 @@ ADI_RADS.A14 = 5.19e-2;
 ADI_RADS.A15 = 5.53e-2;
 ADI_RADS.A16 = 5.74e-2;
 
+%% Range of Information
+roi_rad = 8e-2; % The radius of information is 8cm.
+
 %% Choose scan number to work on
-scan_num = 1;
+scan_num = 2;
 %% Load data
 % Metadata
 metadata = load('../um_bmid/datasets/gen-three/clean/md_list_s11_adi.mat');
@@ -49,9 +52,23 @@ scan_data = scan_data(:, 1:sample_divide:end, :);
 org_signal = squeeze(scan_data(scan_num, :, :));
 org_metadata = metadata{scan_num};
 
+%% Load metadata from signal
+% Get antenna phase radius
+ant_rad = metadata{scan_num}.ant_rad * 1e-2; % The distance from the antenna to the centre.
+ant_phase_rad = ant_rad + 2.4e-2; % Add the distance from the centre to the antenna's phase centre. "2.4cm" was measured by Tyson Reimer. 
+% Get breast tumor
 if isnan(org_metadata.tum_diam)
     disp('This scan does NOT have a tumor!');
+    tum_coords = [0, 0];
+    tum_rad = 0;
+else
+    tum_coords = [ org_metadata.tum_x, org_metadata.tum_y ] * 1e-2;
+    tum_rad = ( org_metadata.tum_diam / 2 ) * 1e-2;
 end
+% Get breast adipose
+adi_coords = [ org_metadata.adi_x, org_metadata.adi_y ] * 1e-2;
+split_adi_id = strsplit(org_metadata.phant_id, 'F'); % Split the string after every "F". This will tell us the adi ID
+adi_rad = ADI_RADS.(split_adi_id{1}); % Radius of the adi-pose. For some reason, it is not included in the metadata.
 
 %% Plot the signal
 figure;
@@ -67,119 +84,56 @@ subplot(2, 1, 2);
 plot(frequencies, channel1_phase);
 xlabel('Frequency (Hz)');
 ylabel('Phase (rad)');
-legend('clean signal'); 
+legend('clean signal');
 
-%% Get signal radius
-ant_rad = metadata{scan_num}.ant_rad * 1e-2; % The distance from the antenna to the centre.
-sig_rad = ant_rad + 2.4e-2; % Add the distance from the centre to the antenna's phase centre. "2.4cm" was measured by Tyson Reimer. 
-%% Adi radius
-split_adi_id = strsplit(org_metadata.phant_id, 'F'); % Split the string after every "F".
-adi_rad = ADI_RADS.(split_adi_id{1}); % Radius of the adi-pose. For some reason, it is not included in the metadata.
-%% Range of Information radius
-roi_rad = 8e-2; % The radius of information is 8cm.
-
-%% Calculate permitivity
-full_xs = linspace(-sig_rad, sig_rad, m_size);
-full_ys = linspace(sig_rad, -sig_rad, m_size);
-
-% Cast these to 2D for ease later
-[ full_pix_xs, full_pix_ys ] = meshgrid(full_xs, full_ys);
-
-% Find the distance from each pixel to the center of the model space
-pix_dist_from_center = sqrt(full_pix_xs.^2 + full_pix_ys.^2);
-
-% Get the region of interest as all the pixels inside the circle-of-interest
-full_in_roi = false([m_size, m_size]);
-full_in_roi(pix_dist_from_center < sig_rad) = true;
-
-air_permitivity = 1;
-adi_permitivity = 7.08;
-tum_permitivity = 77.11;
-skin_permitivity = 40;
-skin_thickness = 0;
-
-tum_x = org_metadata.tum_x * 1e-2;
-tum_rad = ( org_metadata.tum_diam / 2 ) * 1e-2;
-tum_y = org_metadata.tum_y * 1e-2;
-adi_x = org_metadata.adi_x * 1e-2;
-adi_y = org_metadata.adi_y * 1e-2;
-
-% Compute the pixel distances from the center of each tissue
-pix_dist_from_adi = sqrt((full_pix_xs - adi_x).^2 + (full_pix_ys - adi_y).^2);
-pix_dist_from_tum = sqrt((full_pix_xs - tum_x).^2 + (full_pix_ys - tum_y).^2);
-
-% Initialize breast model to be uniform background medium
-breast_model = air_permitivity .* ones([m_size, m_size]);
-
-% Assign the tissue components
-breast_model(pix_dist_from_adi < (adi_rad + skin_thickness) ) = skin_permitivity;
-breast_model(pix_dist_from_adi < adi_rad) = adi_permitivity;
-breast_model(pix_dist_from_tum < tum_rad) = tum_permitivity;
-
-%% Calculate the average speed of a signal
-vac_speed = 3e8;
-prop_speed = mean( ( vac_speed ./ sqrt( breast_model(full_in_roi) ) ), "all" );
+%% Calculate signal speed
+prop_speed = um_bmid.get_signal_speed(...
+    ant_phase_rad, ...
+    adi_rad, ...
+    adi_coords=adi_coords, ...
+    tum_coords=tum_coords, ...
+    tum_rad=tum_rad, ...
+    m_size=m_size ...
+    );
 
 %% Calculate the antenna locations
 number_antennas = size(scan_data, 3); % The number of antenna locations.
-
 starting_antenna_angle = deg2rad(-130);
-antenna_angles = (linspace(0, (1 - (1/number_antennas) ) * 2 * pi, number_antennas)); % If number_antennas = 72, then steps of 5 from 0 to 355 (but in radians)
-antenna_angles = antenna_angles + starting_antenna_angle; % The starting antenna angle is not always 0 degrees.
 
-antenna_locations = permute ( [ ( cos(antenna_angles) * sig_rad ); ( sin(antenna_angles) * sig_rad ) ], [2,1] );
+antenna_locations = um_bmid.get_antenna_locations(...
+    ant_phase_rad, ...
+    number_antennas=number_antennas, ...
+    starting_antenna_angle=starting_antenna_angle...
+    );
 
-%% Calculate the delay for each signal at each position
+%% Create meshgrid for calculations
 %Define the x/y points on each axis
 xs = linspace(-roi_rad, roi_rad, m_size);
 ys = linspace(roi_rad, -roi_rad, m_size);
-
-% Cast these to 2D for ease latersig_rad
+% Cast these to 2D for ease
 [ pix_xs, pix_ys ] = meshgrid(xs, ys);
+
 % Find the distance from each pixel to the center of the model space
 pix_dist_from_center = sqrt(pix_xs.^2 + pix_ys.^2);
 
+%% Calculate region of interest
 % Get the region of interest as all the pixels inside the
 % circle-of-interest
 in_roi = false([m_size, m_size]);
 in_roi(pix_dist_from_center < roi_rad) = true;
 
-% Get the ant x/y positions
-ant_xs = antenna_locations(:, 2);
-ant_ys = antenna_locations(:, 1);
-
-% Init array for storing pixel time-delays
-p_ds = zeros([number_antennas, m_size, m_size]);
-
-for a_pos = 1:number_antennas  % For each antenna position
-
-    % Find x/y position differences of each pixel from antenna
-    x_diffs = pix_xs - ant_xs(a_pos);
-    y_diffs = pix_ys - ant_ys(a_pos);
-    
-    % Calculate one-way time-delay of propagation from antenna to
-    % each pixel
-    p_ds(a_pos, :, :) = sqrt(x_diffs.^2 + y_diffs.^2);
-end
-
-pixel_delay_time = p_ds ./ prop_speed; % Convert distance delay to time delay, relative to permittivity.
-
-% Apply extra time delay for monostatic. Constant taken from Reimer.
-pixel_delay_time = pixel_delay_time + 0.19e-9;
-
+%% Calculate time delay for each signal at each point
+pixel_delay_time = um_bmid.get_delays(prop_speed, m_size, pix_xs, pix_ys, antenna_locations);
 
 %% Calculate phase factor
 pix_ts = pixel_delay_time;
-
 phase_fac = exp(-1i * 2 * pi * frequencies(:) .* reshape(pix_ts, 1, size(pix_ts, 1), size(pix_ts, 2), size(pix_ts, 3)));
 
 %% Perform DAS beamform
 % Convert adi_cal_cropped to have singleton dimensions for broadcasting
 adi_cal_cropped_expanded = reshape(org_signal, size(org_signal, 1), size(org_signal, 2), 1, 1);
-
 % Perform the element-wise multiplication and summation using implicit expansion
 das_adi_recon = sum(sum(adi_cal_cropped_expanded .* phase_fac.^(-2), 1), 2);
-
 % Squeeze the result to remove singleton dimensions
 das_adi_recon = squeeze(das_adi_recon);
 
