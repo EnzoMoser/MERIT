fe036f987e8815469e08073432731eaa74a1f230
Enzo Moser <irlenzom@gmail.com>
2024-08-06 16:56:46 +0000

    Best working comparison between Merit and Reimer

--- create_ummid_render.m
+++ create_ummid_render.m
@@ -1,9 +1,8 @@
-clear
-clc
+%% Values that you can quickly change:
+scan_num = 1; % Choose scan number to work on (there are 200 in total)
+m_size = 100; % Choose the number of pixels for each side. Less is faster:
 
-m_size = 100;
-
-%% ADI Radius - For some reason not in the metadata
+%% The radius for each adipose - For some reason this is not in the metadata
 ADI_RADS.A1 = 3.87e-2;
 ADI_RADS.A2 = 5e-2;
 ADI_RADS.A3 = 5.66e-2;
@@ -17,16 +16,16 @@ ADI_RADS.A16 = 5.74e-2;
 %% Range of Information
 roi_rad = 8e-2; % The radius of information is 8cm.
 
-%% Choose scan number to work on
-scan_num = 1;
 %% Load data
-% Metadata
+%--- Metadata
+% Warning: All values in the metadata are in centimetres!!!
+% Make sure to convert them to metres when using.
 metadata = load('../um_bmid/datasets/gen-three/clean/md_list_s11_adi.mat');
 % Extract the metadata array from the struct dytpe it is stored in
 metadataFieldName = fieldnames(metadata);
 metadata = getfield(metadata, metadataFieldName{1}); %#ok<GFLD>
 
-% Signal data
+%--- Signal data
 scan_data = load('../um_bmid/datasets/gen-three/clean/fd_data_s11_adi.mat');
 % Extract the data array from the struct dtype it is stored in
 scanDataFieldName = fieldnames(scan_data);
@@ -36,27 +35,28 @@ scan_data = getfield(scan_data, scanDataFieldName{1}); %#ok<GFLD>
 frequencies = linspace( 1e9, 9e9, size(scan_data, 2) ); % Frequencies
 frequencies = frequencies(:);
 
-%% Shrink signal frequencies - for better performance
-% Shrink the frequencies for scan_data
+%% Shrink the number of frequencies - for better performance
+%--- Shrink the frequencies for scan_data
 frequency_ids = frequencies >=2e9; % Only include frequencies above this number.
 frequencies = frequencies(frequency_ids);
-% Down-sample freq. Only use every [sample_divide]-th element.
+%--- Down-sample freq. Only use every [sample_divide]-th element.
 sample_divide = 12; % Number to divide the sample by.
 frequencies = frequencies(1:sample_divide:end);
-% Make sure signal frequencies match
+%--- Make sure signal frequencies match
 scan_data = scan_data(:, frequency_ids, :);
 scan_data = scan_data(:, 1:sample_divide:end, :);
 
 %% Load One Signal
-% Get the full signal
+%--- Get the full signal
 org_signal = squeeze(scan_data(scan_num, :, :));
 org_metadata = metadata{scan_num};
 
 %% Load metadata from signal
-% Get antenna phase radius
+%--- Get antenna phase radius
 ant_rad = metadata{scan_num}.ant_rad * 1e-2; % The distance from the antenna to the centre.
 ant_phase_rad = ant_rad + 2.4e-2; % Add the distance from the centre to the antenna's phase centre. "2.4cm" was measured by Tyson Reimer. 
-% Get breast tumor
+
+%--- Get the breast tumor coords and radius
 if isnan(org_metadata.tum_diam)
     disp('This scan does NOT have a tumor!');
     tum_coords = [0, 0];
@@ -65,26 +65,11 @@ else
     tum_coords = [ org_metadata.tum_x, org_metadata.tum_y ] * 1e-2;
     tum_rad = ( org_metadata.tum_diam / 2 ) * 1e-2;
 end
-% Get breast adipose
+
+%--- Get breast adipose
 adi_coords = [ org_metadata.adi_x, org_metadata.adi_y ] * 1e-2;
 split_adi_id = strsplit(org_metadata.phant_id, 'F'); % Split the string after every "F". This will tell us the adi ID
 adi_rad = ADI_RADS.(split_adi_id{1}); % Radius of the adi-pose. For some reason, it is not included in the metadata.
-
-% %% Plot the signal
-% figure;
-% data_channel1 = [ org_signal(:, 1) ] ;
-% channel1_magnitude = mag2db(abs(data_channel1));
-% channel1_phase = unwrap(angle(data_channel1));
-% subplot(2, 1, 1);
-% plot(frequencies, channel1_magnitude);
-% xlabel('Frequency (Hz)');
-% ylabel('Magnitude (dB)');
-% legend('clean signal');
-% subplot(2, 1, 2);
-% plot(frequencies, channel1_phase);
-% xlabel('Frequency (Hz)');
-% ylabel('Phase (rad)');
-% legend('clean signal');
 
 %% Calculate signal speed
 prop_speed = um_bmid.get_signal_speed(...
@@ -92,8 +77,7 @@ prop_speed = um_bmid.get_signal_speed(...
     adi_rad, ...
     adi_coords=adi_coords, ...
     tum_coords=tum_coords, ...
-    tum_rad=tum_rad, ...
-    m_size=m_size ...
+    tum_rad=tum_rad ...
     );
 
 %% Calculate the antenna locations
@@ -132,9 +116,11 @@ phase_fac = exp(-2i * pi * frequencies(:) .* reshape(pix_ts, 1, size(pix_ts, 1),
 %% Perform DAS beamform
 % Convert adi_cal_cropped to have singleton dimensions for broadcasting
 adi_cal_cropped_expanded = org_signal;
+
 % Perform the element-wise multiplication and summation using implicit expansion
 das_adi_recon_temp1 = adi_cal_cropped_expanded .* phase_fac.^2;
 das_adi_recon = sum(sum(das_adi_recon_temp1, 1), 2);
+
 % Squeeze the result to remove singleton dimensions
 das_adi_recon = squeeze(das_adi_recon);
 
@@ -153,6 +139,22 @@ new_das = merit.beamform(org_signal, frequencies(:), points, apple, um_bmid.DAS)
 % Convert to grid for image display
 grid_ = merit.domain.img2grid(squeeze(abs(new_das)), points, axes_{:});
 
+figure()
+imagesc(axes_{:}, rot90(grid_, 2));
+axis equal
+colormap("jet");
+colorbar
+% Matlab likes to display the Y Direction backwards. So we tell it not to:
+set(gca,'YDir','normal'); set(gca,'XDir','normal');
+
+figure()
+imagesc(axes_{:}, squeeze(abs(das_adi_recon)) );
+axis equal
+colormap("jet");
+colorbar
+% Matlab likes to display the Y Direction backwards. So we tell it not to:
+set(gca,'YDir','normal'); set(gca,'XDir','normal');
+
 %% Plot image
 img = rot90(grid_, 2);
 img_to_plt = img .* ones(size(img), "like", img);
@@ -161,9 +163,6 @@ img_to_plt( ~in_roi ) = nan;
 
 figure()  % Make the figure window
 hold on
-
-axes_{1} = xs;
-axes_{2} = ys;
 
 imagesc(axes_{:}, img_to_plt);
 colormap("jet");
@@ -182,13 +181,10 @@ img_to_plt( ~in_roi ) = nan;
 figure()  % Make the figure window
 hold on
 
-axes_{1} = xs;
-axes_{2} = ys;
-
 imagesc(axes_{:}, img_to_plt);
 colormap("jet");
 colorbar
 title("reimer")
 axis equal
-clim(lims)
+% clim(lims)
 hold off
